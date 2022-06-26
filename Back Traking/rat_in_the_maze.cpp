6c4529b9a181d209fa9fd83657c3cfe6f83a78e6
Priyabrat Duarah <prybrt06@gmail.com>
2022-06-26 20:06:22 +0530

    Complete solution of rat in the maze problem has been uploaded

--- Back Traking/rat_in_the_maze.cpp
+++ Back Traking/rat_in_the_maze.cpp
@@ -1,91 +1,91 @@
 #include<iostream>
 using namespace std;
 
-bool isPossible(int **arr,bool **visit,int n,int row,int column)
+void printSolution(int **visited,int n)
 {
-    if(row<n)
-    {
-        if(arr[row][column]==1)
-        {
-            if(visit[row][column]==false)
-            {
-                return true;
-            }
-        }
-    }
-
-    if(column<n)
-    {
-        if(arr[row][column]==1)
-        {
-            if(visit[row][column]==false)
-            {
-                return true;
-            }
-        }
-    }
-    
-
-    return false;
-}
-
-void helper(int n,int arr,int row,int column)
-{
-    
-}
-
-void solveRatInMaze(int n,int **arr)
-{
-    bool **visit = new bool*[n];
-
-    for(int i=0;i<n;i++)
-    {
-        visit[i] = new bool[n];
-        for(int j=0;j<n;j++)
-        {
-            visit[i][j] = false;
-        }
-    }
-
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
-            if((i+1)!=n)
-            {
-                if(isPossible(arr,visit,n,i+1,j))
-                {
-                    visit[i+1][j] = true;
-                    solveRatInMaze(n,arr);
-                }
-            }
-            
+            cout << visited[i][j] << " ";
         }
     }
+
+    cout << endl;
+}
+
+void mazeHelp(int **arr,int **visited,int n,int row,int column)
+{
+    
+    if(row==n-1 && column==n-1)
+    {
+        visited[row][column] = 1;
+        printSolution(visited,n);
+        visited[row][column] = 0;
+
+        return;
+    }
+
+    if(row==n || row<0 || column==n || column<0 || arr[row][column]==0 || visited[row][column]==1)
+    {
+        return;
+    }
+
+    visited[row][column] = 1;
+    mazeHelp(arr,visited,n,row+1,column);
+    mazeHelp(arr,visited,n,row,column+1);
+    mazeHelp(arr,visited,n,row-1,column);
+    mazeHelp(arr,visited,n,row,column-1);
+    visited[row][column] = 0;
+}
+
+void solveRatInMaze(int **arr,int n)
+{
+    int **visited = new int*[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        visited[i] = new int[n];
+        for(int j=0;j<n;j++)
+        {
+            visited[i][j] = 0;
+        }
+    }
+
+    mazeHelp(arr,visited,n,0,0);
+
+    for(int i=0;i<n;i++)
+    {
+        delete [] visited[i];
+    }
+
+    delete [] visited;
 }
 
 int main()
 {
-    int a;
-    cin>>a;
+    int n;
+    cin>>n;
 
-    int **arr = new int*[a];
+    int **arr = new int*[n];
 
-    for(int i=0;i<a;i++)
+    for(int i=0;i<n;i++)
     {
-        arr[i] = new int[a];
-        for(int j=0;j<a;j++)
+        arr[i] = new int[n];
+        for(int j=0;j<n;j++)
         {
             cin>>arr[i][j];
         }
     }
 
-    solveRatInMaze(a,arr);
+    solveRatInMaze(arr,n);
 
-    for(int i=0;i<a;i++)
+    for(int i=0;i<n;i++)
     {
         delete [] arr[i];
     }
 
     delete [] arr;
+
+    return 0;
 }
